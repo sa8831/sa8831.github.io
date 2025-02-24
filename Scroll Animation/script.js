a6d343e1c2682bfbaa930fae6c9ae35c07c57dbd
sa8831 <sa8831@nyu.edu>
2025-02-25 01:58:31 +0400

    FILES

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -7,23 +7,15 @@ thirdButton.addEventListener('click', scrollPage);
 fourthButton.addEventListener('click', scrollPage);
 
 function scrollPage(){
-    // this keyword returns the HTML element that triggered the event (in this case, click event)
-    // Check id of this HTML element
-    // If id name is 'third-page'
     if(this.id === 'third-page'){
         window.scrollTo({
-            // Scroll twice the height of window height
             top: window.innerHeight * 2,
             left: 0,
-            // Setting behavior to 'smooth' will animate the scroll
             behavior: 'smooth'
         });
     }
-
-    // If id name is 'fourth-page'
     if(this.id === 'fourth-page'){
         window.scrollTo({
-            // Scroll three times the height of window height
             top: window.innerHeight * 3,
             left: 0,
             behavior: 'smooth'
@@ -31,11 +23,11 @@ function scrollPage(){
     }
 }
 
-// An array of stories as strings
+// Storytelling functionality
 let stories = [
     'Hello!',
     'I am a cat, your storyteller.',
-    'I am here to help you understand the world',
+    'I am here to help you understand the world.',
     'It is a fictional world, but a world nonetheless.'
 ];
 
@@ -43,38 +35,22 @@ let catDiv = document.getElementById('BigC-div');
 let catText = document.getElementById('BigC-text');
 let navButtons = document.getElementsByClassName('nav-btn');
 
-// Add scroll event for the whole HTML document and set changeText function as callback
 document.addEventListener('scroll', changeText);
 
-// changeText function runs whenever scroll happens on the web page
 function changeText(){
-    // pos saves current scrollY position
     let pos = window.scrollY;
-    // width saves current web page width
     let width = window.innerWidth;
-    // height saves current web page height
     let height = window.innerHeight;
-    // Calculate which section the user has scrolled to
-    // parseInt() function used to ignore decimals
     let sectionNum = parseInt(pos / height);
 
-    // Set the text of cat-text element depending on which section the user has currently scrolled to
     catText.innerHTML = stories[sectionNum];
 
-    // Calculate the horizontal position for the cat to slide
-    // Assuming you want the cat to slide from left (0px) to right (width - catDiv width)
     let maxSlide = width - catDiv.offsetWidth;
     let slidePosition = (pos / (height * 4)) * maxSlide;
-
-    // Set the left position of catDiv
     catDiv.style.left = slidePosition + 'px';
 
-    // Fix color of navigation button
-    // Set all button color to white
     for(let i = 0; i < navButtons.length; i++){
         navButtons[i].style.backgroundColor = 'white';
     }
-
-    // Set color of current section button to black
     navButtons[sectionNum].style.backgroundColor = 'black';
 }
